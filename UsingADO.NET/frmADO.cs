4e2d65baac9ed02eda047f4e51c6c5422f6d0d0c
Doan Quang Huy <quanghuybest@gmail.com>
2022-05-31 20:28:49 +0700

    upload project

---
+++ UsingADO.NET/frmADO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingADO.NET
+{
+    public partial class frmADO : Form
+    {
+        public frmADO()
+        {
+            InitializeComponent();
+        }
+    }
+}
